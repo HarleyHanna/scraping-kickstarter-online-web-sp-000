6f3bc5a7067e782cf719f3ad373302f5098bded1
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-06 21:55:36 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,7 @@ def create_project_hash
   #projects: kickstarter.css("li.project.grid_4")
   #titles: project.css("h2.bbcard_name strong a").text 
   #project.css("div.project-thumbnail a img").attribute("src").value
+  #project.css("p.bbcard_blurb").text
 end
 
 
