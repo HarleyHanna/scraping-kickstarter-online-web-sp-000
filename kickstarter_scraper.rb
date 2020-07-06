d3d18cd29a3841736c76ca223fb27f609c263bc0
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-06 21:53:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,16 @@
-# require libraries/modules here
+require 'pry'
+require 'nokogiri'
+
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry 
+  #projects: kickstarter.css("li.project.grid_4")
+  #titles: project.css("h2.bbcard_name strong a").text 
+  #project.css("div.project-thumbnail a img").attribute("src").value
 end
+
+
+create_project_hash
